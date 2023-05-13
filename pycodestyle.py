57910a5dd83d5d5f8f9d715c85245994a5a05183
Chinemerem Nwaka <ccnwaka1988@gmail.com>
2023-05-13 18:23:05 +0200

    added pycodestyle.py file

---
+++ pycodestyle.py
@@ -0,0 +1,9 @@
+class MyClass:
+    def __init__(self, my_var):
+        self.my_var = my_var
+
+    def my_method(self):
+        print("My method was called.")
+
+    def my_other_method(self, my_other_var):
+        print(f"My other variable is {my_other_var}.")
